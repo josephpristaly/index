ac6ec2080cd064147c156677f44eacde1287a87f
Tahmina Zaman Tuly <67723201+josephpristaly@users.noreply.github.com>
2024-05-07 19:33:10 +0600

    .

--- src/components/FourOhFour.tsx
+++ src/components/FourOhFour.tsx
@@ -16,16 +16,16 @@ const FourOhFour: React.FC<{ errorMsg: string }> = ({ errorMsg }) => {
         <div className="text-sm">
           Press{' '}
           <kbd className="rounded border border-gray-400/20 bg-gray-100 px-1 font-mono text-xs dark:bg-gray-800">
-            F12
+          ৪০৪
           </kbd>{' '}
           and open devtools for more details, and report this issue to{' '}
           <a
             className="text-blue-600 hover:text-blue-700 hover:underline"
-            href="https://github.com/lyc8503/onedrive-cf-index-ng/issues"
+            href="https://fb.me/ZahidIUBAT"
             target="_blank"
             rel="noopener noreferrer"
           >
-            onedrive-cf-index-ng issues
+            contact with me
           </a>
           .
         </div>
