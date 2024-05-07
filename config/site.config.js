f745e4612caa029040b778c68bdbd490462ba5aa
Tahmina Zaman Tuly <67723201+josephpristaly@users.noreply.github.com>
2024-05-07 19:07:47 +0600

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -49,8 +49,8 @@ module.exports = {
   // you provide. See the documentation for details.
   links: [
     {
-      name: 'GitHub',
-      link: 'https://github.com/ZahidIUBAT',
+      name: "",
+      link: "",
     },
   ],
 
